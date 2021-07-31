88bdb9d9a9404ce4289f6980f524749415948f2e
Tom Rives <tom.rives@epitech.eu>
2021-07-31 23:57:53 +0200

    [ADD] error handled / [ADD] data format default

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,31 @@
 const config = require('../config.json');
 
 exports.checkServer = (req, res) => {
+    
+    let data = {
+        "frames": [
+            {
+                "text": "Server",
+                "icon": config.icon.unknown
+            },
+            {
+                "text": "OFFLINE",
+                "icon": config.icon.unknown
+            }
+        ]
+    }
+
+    if (!req.query.hasOwnProperty("name") ||
+        !req.query.hasOwnProperty("type") ||
+        !req.query.hasOwnProperty("address"))
+        return res.status(400).json({msg: "Missing parameters"});
+    if (req.query["name"] !== '')
+        data.frames[0].text = req.query["name"];
+    if (req.query["type"] == '' ||
+        !["Minecraft", "Source", "FiveM"].includes(req.query["type"]))
+        return res.status(200).json(data);
+    if (req.query["address"] == '')
+        return res.status(200).json(data);
     console.log(req.query);
     return res.status(200).json({msg: "OK"});
 }
