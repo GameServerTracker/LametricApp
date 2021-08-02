54206d107cdf8471416b00fd8a3331c270025c3c
Tom Rives <tom.rives@epitech.eu>
2021-08-02 21:30:53 +0200

    [EDIT] Tests manage server

--- test/fivem.js
+++ test/fivem.js
@@ -1,20 +1,14 @@
 const fiveM = require('fivem');
 
-const srv = new fiveM.Server('185.157.247.67:30120');
+const srv = new fiveM.Server('ip');
 
 srv.getPlayers()
-.then(data => {
-    const p = data;
-    srv.getMaxPlayers()
     .then(data => {
-        console.log(`${p} / ${data}`);
+        const p = data;
+        srv.getMaxPlayers()
+            .then(data => {
+                console.log(`${p} / ${data}`);
+            })
+            .catch(err => { console.error(err); })
     })
-    .catch(err => {
-        console.error("SALE CON !");
-        console.error(err);
-    })
-})
-.catch(err => {
-    console.log("FUK");
-    console.error(err);
-})
+    .catch(err => { console.error(err); })
--- test/minecraft.js
+++ test/minecraft.js
@@ -1,10 +1,16 @@
 const mc = require("minecraft-server-ping");
 
-mc.ping("164.132.206.97")
-.then(data => {
-    console.log(`${data.players.online} / ${data.players.max}`);
-})
-.catch(err => {
-    console.log("FUCK");
-    console.log(err);
-})
+const ip = "ip";
+let port = 25565; // Default port for Minecraft server : 25565
+const ipTab = ip.split(':');
+
+if (ipTab[1] != undefined)
+    port = ipTab[1];
+
+mc.ping(ipTab[0], port)
+    .then(data => {
+        console.log(`${data.players.online} / ${data.players.max}`);
+    })
+    .catch(err => {
+        console.log(err);
+    })
