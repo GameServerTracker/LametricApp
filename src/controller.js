83c40780afe5328b70fb453f6b246a7df0465ab4
Tom Rives <tom.rives@epitech.eu>
2021-08-02 18:29:39 +0200

    [ADD] Manage request server status for minecraft server

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,9 @@
 const config = require('../config.json');
+const server = require('./serverStatus');
+
+const typeTab = ["Minecraft", "Source", "FiveM"];
 
 exports.checkServer = (req, res) => {
-    
     let data = {
         "frames": [
             {
@@ -18,14 +20,27 @@ exports.checkServer = (req, res) => {
     if (!req.query.hasOwnProperty("name") ||
         !req.query.hasOwnProperty("type") ||
         !req.query.hasOwnProperty("address"))
-        return res.status(400).json({msg: "Missing parameters"});
+        return res.status(400).json({ msg: "Missing parameters" });
     if (req.query["name"] !== '')
         data.frames[0].text = req.query["name"];
     if (req.query["type"] == '' ||
-        !["Minecraft", "Source", "FiveM"].includes(req.query["type"]))
+        !typeTab.includes(req.query["type"]))
         return res.status(200).json(data);
     if (req.query["address"] == '')
         return res.status(200).json(data);
     console.log(req.query);
-    return res.status(200).json({msg: "OK"});
+
+    if (req.query["type"] === "Minecraft") {
+        server.minecraft(req.query["address"])
+            .then(result => {
+                console.log(result);
+                data.frames[1].text = result;
+                console.log("Hummmm ok")
+                return res.status(200).json(data);
+            })
+            .catch(err => {
+                console.error(err);
+                return res.status(200).json(data);
+            });
+    }
 }
---
+++ src/serverStatus.js
@@ -0,0 +1,35 @@
+const mc = require("minecraft-server-ping");
+const ssq = require('ssq');
+const fiveM = require('fivem');
+
+exports.minecraft = (address) => {
+    console.log("Ceci est un serveur Minecraft !!!");
+    const ipTab = address.split(':');
+    let port = 25565;
+
+    return new Promise((resolve, reject) => {
+        if (ipTab[1] != undefined)
+            port = ipTab[1];
+        if (port < 0 || port > 65536)
+            reject("Bad port : port should be > 0 and < 65536.");
+        console.log(`CHECK IP: ${ipTab[0]}:${port}`);
+        mc.ping(ipTab[0], port)
+            .then(data => {
+                console.log("GO");
+                console.log(data);
+                resolve(`${data.players.online} / ${data.players.max}`);
+            })
+            .catch(err => {
+                console.error("Hé bah non")
+                reject(err);
+            })
+    })
+}
+
+exports.source = (address) => {
+    let port = 27015;
+}
+
+exports.five = (address) => {
+
+}
