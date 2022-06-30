185f6bff4a150e1303d3fbe7ff3282d11e0132b4
Tom Rives <tom.rives@epitech.eu>
2022-06-30 13:45:21 +0200

    [ADD] track Minecraft/source server

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,16 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { ping } from 'minecraft-server-ping';
+import { IMinecraftData } from 'minecraft-server-ping/dist/interfaces';
+import query from 'source-server-query';
 
 @Injectable()
 export class TrackService {
-    async trackMinecraftServer(address: string) : Promise<string> {
-        return "0 / 0";
+    async trackMinecraftServer(address: string): Promise<string> {
+        const addressSplited: string[] = address.split(':');
+        const hostname = addressSplited[0];
+        const port: number = addressSplited[1] != undefined ? +addressSplited[1] : 25565;
+        const optionPing: any = { timeout: 1000 }
+
+        try {
+            if (port < 0 || port > 65536 || isNaN(port))
+                throw (`Address ${address} has a bad port !`);
+            const data: IMinecraftData = await ping(hostname, port, optionPing);
+            return `${data.players.online || 0} / ${data.players.max || 0}`;
+        } catch (err: any) {
+            console.error(err);
+            return "OFFLINE";
+        }
     }
 
-    async trackSourceServer(address: string) : Promise<string> {
-        return "0 / 0";
+    async trackSourceServer(address: string): Promise<string> {
+        const addressSplited: string[] = address.split(':');
+        const hostname = addressSplited[0];
+        const port: number = addressSplited[1] != undefined ? +addressSplited[1] : 27015;
+
+        try {
+            if (port < 0 || port > 65536 || isNaN(port))
+                throw (`Address ${address} has a bad port !`);
+            const data: any = await query.info(hostname, port, 1000);
+            return `${data.players || 0} / ${data.max_players || 0}`;
+        } catch (err: any) {
+            console.error(err);
+            return "OFFLINE";
+        }
     }
 
-    async trackFiveMServer(address: string) : Promise<string> {
+    async trackFiveMServer(address: string): Promise<string> {
         return "0 / 0";
     }
 }
