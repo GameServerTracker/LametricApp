3e3b7122abc2bc3cc80af1e95647fdfd36449965
Tom Rives <tom.rives@epitech.eu>
2022-12-23 16:40:35 +0900

    [FIX] Bind port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: false, whitelist: true }))
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
