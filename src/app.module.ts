035c91a4e5d9b0b6cf2b755c3df8a707b284bf56
Tom Rives <tom.rives@epitech.eu>
2023-08-26 23:04:24 +0200

    [DELETE] Useless Module on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServerModule } from './server/server.module';
-import { TrackService } from './track/track.service';
-import { TrackModule } from './track/track.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
-  imports: [ServerModule, TrackModule],
+  imports: [ServerModule],
   controllers: [AppController],
-  providers: [AppService, TrackService],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
