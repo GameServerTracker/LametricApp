0b5f65b5cbf14bce9b081e590d186828869bbf08
Tom Rives <tom.rives@epitech.eu>
2024-01-13 22:28:33 +0100

    [EDIT] hello world

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): any {
-    return { message: 'GameServerTracker 3.0 is OK !' };
+    return { message: 'GameServerTracker Lametric App is OP !' };
   }
 }
